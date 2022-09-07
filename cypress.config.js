fed71f63b72d1e12e2f77e87c72114958d69e5de
renatamg10 <renata.mglb@gmail.com>
2022-09-07 14:28:01 -0300

    inclusao mochawesome reporter no projeto

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
   e2e: {
     //ambiente de teste
     baseUrl: 'https://buger-eats-qa.vercel.app',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      reporter: 'mochawesome'
     },
    
   }, 
